e398bdfeb0a91b10374521618d75079828bd00c2
Hihlo Ivan <ivanlittle8@gmail.com>
2024-11-28 18:04:12 +0200

    Add addSum method to calculate sum of two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub Desktop!");
+        int sum = addSum(5, 3);
+        System.out.println("Sum is: " + sum);
+    }
+
+    public static int addSum(int a, int b) {
+        return a + b;
     }
 }
